e7f0ce3ef6a4cd56c8d3c1f0b02121336ff9a12f
Meder <m.toktosunov@icloud.com>
2015-12-07 11:33:10 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'sinatra-activerecord'
+gem 'pg'
+gem 'rake'
+
+
+group :test, :development do
+  gem 'pry'
+  gem 'rspec'
+end
---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+require 'pry'
+
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :index
+end
+
+
+get "/contacts/:id" do
+  @contact = Contact.find(params[:id])
+  erb :show
+end
